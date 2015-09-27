0707abf5e4c9423a14d8151a61414ef7499abb61
C.H.A.D.o <chado@chado.by>
2015-09-27 13:17:04 +0300

    Add Challenge strut prototype

--- Challenge/ChallengesController.swift
+++ Challenge/ChallengesController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
+
+// ToDo: разобраться с датой, учитывая часовой пояс
+
+struct Challenge {
+    var num: UInt?;
+    var start: timeb?;
+    var end: timeb;
+    var action: String;
+    var item: String?;
+    var place: String?;
+    var person: String?;
+    
+    init(num: UInt?, start: timeb?, end: timeb,
+        action: String, item: String?, place: String?, person: String?) {
+            self.num = num;
+            self.start = start;
+            self.end = end;
+            self.action = action;
+            self.item = item;
+            self.place = place;
+            self.person = person;
+    }
+}
+
+
 class ChallengesController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var ChallengesTable: UITableView!
-    
-    let textCellIdentifier = "OneRow"
     
     var ChallengesArray = [String]()
 
@@ -20,7 +43,7 @@ class ChallengesController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for index in 1...122 {
+        for index in 1...62 {
             ChallengesArray.append("Quest #\(index)")
         }
         
@@ -42,7 +65,7 @@ class ChallengesController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = ChallengesTable.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! ChallengesTableViewCell
+        let cell = ChallengesTable.dequeueReusableCellWithIdentifier("OneRow", forIndexPath: indexPath) as! ChallengesTableViewCell
         
         let row = indexPath.row
         cell.HeaderLabel?.text = ChallengesArray[row]
@@ -50,7 +73,9 @@ class ChallengesController: UIViewController, UITableViewDataSource, UITableView
         let minutes = arc4random_uniform(98) + 1;
         
         if (minutes > 70) {
-            cell.selected = true;
+            cell.backgroundColor = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1);
+            cell.TimerLabel.text = "Start in \(minutes) minute";
+            cell.ProgressBar.progress = Float(minutes) / 100.0;
         } else {
             cell.TimerLabel.text = "Ends in \(minutes) minute";
             cell.ProgressBar.progress = 1 - Float(minutes) / 100.0;
